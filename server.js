901c21c90f4d7deb760d4eb8a6498a2a14a9b796
Richard Buagbe <buagbe48@gmail.com>
2026-05-12 12:24:45 +0000

    improve

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: true,
+    origin: ["https://klouse-frontend.vercel.app", "http://localhost:5173"],
     credentials: true,
   }),
 );
--- socket.js
+++ socket.js
@@ -6,7 +6,7 @@ const onlineUsers = {};
 function initSocket(server) {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: true,
       methods: ["GET", "POST"],
       credentials: true,
     },
